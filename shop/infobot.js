ba2f99334a3c771dcf10f4ea6c79879dd251b2fa
Wincans <94646426+Wincans@users.noreply.github.com>
2022-02-02 11:54:30 +0700

    Update infobot.js

--- shop/infobot.js
+++ shop/infobot.js
@@ -27,13 +27,13 @@ return `ℒ⃝👾 *B O T  S T A T I S T I C*
 ℒ⃝👨‍💻 *C O N N E C T  W I T H  M E*
 
 ⛎ Api : api-alphabot.herokuapp.com
-♈ YouTube : youtube.com/c/zeeoneofc
-♉ Github : zeeoneofc.github.io
-♊ Instagram : instagram.com/zeeoneofc
-♋ Tiktok : tiktok.com/@zeeoneofc
-♌ Telegram : t.me/zeeoneofc
-♎ Ch Telegram : t.me/zeeone_ofc
-♏ WhatsApp : bit.ly/zeeone_ofc
+♈ YouTube : youtube.com/c/melzofc
+♉ Github : melz.github.io
+♊ Instagram : instagram.com/Melzcntip
+♋ Tiktok : tiktok.com/@Melzcntip
+♌ Telegram : t.me/Melzcntip
+♎ Ch Telegram : t.me/MelzCntip
+♏ WhatsApp : bit.ly/Melzcntip
 `}
 
 exports.infobot = infobot
